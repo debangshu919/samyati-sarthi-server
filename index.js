3777d327a8da0466d958ef32d2a16bc9f81a0d33
itz-me-debangshu <personal.debangshu444@gmail.com>
2025-04-15 10:39:22 +0530

    updated API response structure

--- index.js
+++ index.js
@@ -1,30 +1,56 @@
-import express, { Router } from 'express';
+import express, { request, Router } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { PORT } from './netlify/functions/config.js';
-import chatbot from './netlify/functions/assistant.js';
+import { PORT } from './config.js';
+import { chatbot } from './assistant.js';
 
+const router = Router();
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
 // API ROUTES
-app.get('/chatbot', async (req, res) => {
+router.get('/chatbot', async (req, res) => {
     const { prompt } = req.query;
 
     if (!prompt) {
-        return res.status(400).json({ error: 'Prompt is required' });
+        return res.status(400).json({
+            statusCode: 400,
+            status: 'error',
+            message: 'Prompt is required',
+            request: {
+                prompt: prompt,
+            },
+            response: 'Prompt is required',
+        });
     }
 
     const response = await chatbot(prompt);
     if (response.error) {
-        return res.status(500).json({ error: response.error });
+        return res.status(500).json({
+            statusCode: 500,
+            status: 'error',
+            message: 'Internal server error',
+            request: {
+                prompt: prompt,
+            },
+            response: response.error ? response.error : 'An error occurred while processing your request',
+        });
     }
-    return res.status(200).json({ response });
 
-
+    return res.status(200).json({
+        statusCode: 200,
+        status: 'success',
+        message: 'Data fetched successfully',
+        request: {
+            prompt: prompt,
+        },
+        response: response,
+    });
 });
+
+app.use('/api/', router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,7 +2,6 @@ import express, { Router } from 'express';
 import serverless from "serverless-http";
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { PORT } from '../../config.js';
 import { chatbot } from '../../assistant.js';
 
 const router = Router();
@@ -16,21 +15,40 @@ router.get('/chatbot', async (req, res) => {
     const { prompt } = req.query;
 
     if (!prompt) {
-        return res.status(400).json({ error: 'Prompt is required' });
+        return res.status(400).json({
+            statusCode: 400,
+            status: 'error',
+            message: 'Prompt is required',
+            request: {
+                prompt: prompt,
+            },
+            response: 'Prompt is required',
+        });
     }
 
     const response = await chatbot(prompt);
     if (response.error) {
-        return res.status(500).json({ error: response.error });
+        return res.status(500).json({
+            statusCode: 500,
+            status: 'error',
+            message: 'Internal server error',
+            request: {
+                prompt: prompt,
+            },
+            response: response.error ? response.error : 'An error occurred while processing your request',
+        });
     }
-    return res.status(200).json({ response });
 
-
+    return res.status(200).json({
+        statusCode: 200,
+        status: 'success',
+        message: 'Data fetched successfully',
+        request: {
+            prompt: prompt,
+        },
+        response: response,
+    });
 });
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// })
 
 app.use('/api/', router);
 export const handler = serverless(app);
